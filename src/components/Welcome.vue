583b38f5ad2098dd45444eade71e61ffb69b8a47
Rajat Rana <rajat.rana@vidyamantra.com>
2022-11-03 15:20:50 +0530

    added Welcome component

---
+++ src/components/Welcome.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import {ref, onMounted, computed, watch} from "vue"
+
+const name = ref<string>("");
+
+watch(name, (newName) => {
+  localStorage.setItem("name", newName);
+})
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+})
+</script>
+
+<template>
+  <section class="greeting">
+    <h2 class="title">
+      What's up, <input type="text" placeholder="Name here" v-model="name" />
+    </h2>
+  </section>
+</template>
